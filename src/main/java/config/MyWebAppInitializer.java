a6e4821748eb7e3dee07210e5d0fa518b7ce0370
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-12 11:30:21 +0530

    complete spring and hybernate configurations

---
+++ src/main/java/config/MyWebAppInitializer.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import config.WebConfig;
+
+
+// this is the servelet initializer
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    //final class of spring and hybernate configuration
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,22 @@
+package config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "controller" })
+
+//this is the web configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
